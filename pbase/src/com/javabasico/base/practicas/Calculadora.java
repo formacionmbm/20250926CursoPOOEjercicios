f35df7fd20411731f25a3949549124b43505d020
cesar <ejercicios@gmail.com>
2025-12-01 14:06:43 +0100

    cambios en cesar

--- pbase/src/com/javabasico/base/practicas/Calculadora.java
+++ pbase/src/com/javabasico/base/practicas/Calculadora.java
@@ -1,13 +1,14 @@
 package com.javabasico.base.practicas;
 
-import static com.javabasico.base.practicas.Constantes.SUMA;
+import static com.javabasico.base.practicas.Constantes.*;
+
 /**
- * Calculadora, se realizan las operaciones básicas mediante metodos estáticos
+ * Calculadora, se realizan las operaciones bï¿½sicas mediante metodos estï¿½ticos
  * @author MARIA
  *
  */
 public class Calculadora {
-	//Añadir las constantes para que no existan cadenas en los sysos
+	//Aï¿½adir las constantes para que no existan cadenas en los sysos
 	/**
 	 * Suma
 	 * @param operando1
@@ -26,7 +27,7 @@ public class Calculadora {
 	 */
 	public static void resta(int operando1,int operando2) {
 		int resta = operando1-operando2;
-		System.out.println("RESTA:"+resta);
+		System.out.println(RESTA +resta);
 	}
 
 	/**
@@ -36,7 +37,7 @@ public class Calculadora {
 	 */
 	public static void multiplicacion(int operando1,int operando2) {
 		long multiplicacion = operando1*operando2;
-		System.out.println("MULTIPLICACION"+multiplicacion);
+		System.out.println(MULTIPLICACION+multiplicacion);
 	}
 
 	/**
@@ -46,10 +47,10 @@ public class Calculadora {
 	 */
 	public static void division(int operando1,int operando2) {
 		double division = operando1/operando2;
-		System.out.println("DIVISION"+division);
+		System.out.println(DIVISION+division);
 	}
-	//Añadir un método no estático que realize la suma
-	//NO SE SUELE HACER UNA SOBRECARGA DE METODOS ENTRE ESTATICOS Y NO ESTÁTICOS
+	//Aï¿½adir un mï¿½todo no estï¿½tico que realize la suma
+	//NO SE SUELE HACER UNA SOBRECARGA DE METODOS ENTRE ESTATICOS Y NO ESTï¿½TICOS
 	
 	
 	
