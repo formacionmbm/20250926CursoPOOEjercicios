945b3528a2251fc4888362ad215b9f268bd49df4
cesar <ejercicios@gmail.com>
2025-12-02 12:15:51 +0100

    cambios en perfil

--- pbase/src/com/javabasico/base/practicas/GesPerfil.java
+++ pbase/src/com/javabasico/base/practicas/GesPerfil.java
@@ -1,8 +1,43 @@
 package com.javabasico.base.practicas;
 
-//Implementar el código necesario para realizar un CRUD de Perfil. Realizar una clase de negocio
+import java.util.ArrayList;
+import java.util.List;
+
+//Implementar el cï¿½digo necesario para realizar un CRUD de Perfil. Realizar una clase de negocio
 public class GesPerfil  {
+
+    private List<Perfil> perfiles = new ArrayList<>();// esto es para poder almacenar perfiles, falseando una BBDD
+    private long contadorId = 0; // esto es para que la asignacion de ids empiece en 0
 	
-	
+	public Perfil createPerfil(String nombre){
+        Perfil nuevo = new Perfil(contadorId++, nombre);
+        perfiles.add(nuevo);
+        return nuevo;
+    }
+
+    public Perfil getPerfilById(long id){
+        for (Perfil p : perfiles){ // esto tendrÃ­a que meterlo en un try catch
+            if(p.getId() == id) return p;
+        }
+        return null;
+    }
+
+    public List<Perfil> getAllPerfiles(){
+        return perfiles;
+    }
+
+    public boolean updatePerfil(long id, String nuevoNombre){
+        Perfil p = getPerfilById(id);
+        if(p != null){
+            p.setNombre(nuevoNombre);
+            return  true;
+        }
+        return false;
+    }
+
+    public boolean deletePerfil(long id){
+        return perfiles.removeIf(p -> p.getId() == id);
+    }
+
 
 }
--- pbase/src/com/javabasico/base/practicas/Perfil.java
+++ pbase/src/com/javabasico/base/practicas/Perfil.java
@@ -7,16 +7,32 @@ public class Perfil implements Serializable{
 	
 	// Construir un Bean Perfil, que tenga como atributos  el identificador  y el nombre
 	
-	
+	private long id;
+    private String nombre;
 	//Constructor defecto
-	
-	
-	//Getter//setter
 
-	
-	
-	
-	
-	
-	
+    public Perfil(long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+
+    //Getter//setter
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
