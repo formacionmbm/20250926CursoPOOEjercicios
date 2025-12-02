ef9067ec909beddb02c233ab54da503ba8270d21
formacionmbm <formacionmbm@gmail.com>
2025-12-02 13:57:05 +0100

    implementacion de la simulación de un CRUD de Perfil

---
+++ pbase/src/TestGesPerfil.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javabasico.base.practicas.GesPerfil;
+import com.javabasico.base.practicas.Perfil;
+
+public class TestGesPerfil {
+
+	
+	
+	public static void main(String[] args) {
+		
+		GesPerfil servicio= new GesPerfil();
+		
+		//Recogemos los perfiles de la BBDD
+		List<Perfil> perfiles = servicio.findAll();
+		
+		System.out.println("Lista de perfiles:"+perfiles);
+		
+		//Insertamos un perfil 
+		
+		Perfil perfilAux = new Perfil();
+		
+		perfilAux.setNombre("Usuario");
+		
+		servicio.save(perfilAux);
+		
+		
+		//Insertamos otro perfil		
+		
+		perfilAux = new Perfil();
+		
+		perfilAux.setNombre("Admin");
+		
+		servicio.save(perfilAux);
+		
+		
+		//Recogemos los perfiles de la BBDD
+		perfiles = servicio.findAll();
+		
+		System.out.println("Lista de perfiles:"+perfiles);
+		
+		
+		
+	}
+}
