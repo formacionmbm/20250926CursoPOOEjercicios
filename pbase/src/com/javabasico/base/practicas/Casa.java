039ae9b2c50097a7076d46aea83b02d23e5ab458
Marcos <viacepeda@gmail.com>
2025-12-02 11:10:04 +0100

    Primera Ascensión

--- pbase/src/com/javabasico/base/practicas/Casa.java
+++ pbase/src/com/javabasico/base/practicas/Casa.java
@@ -3,37 +3,96 @@ package com.javabasico.base.practicas;
 
 public class Casa {
 	//Paso1: Inicializamos las variables de entero: numHabitaciones y planta por defecto a 0
+	private int numHabitaciones;
+	private int planta;
 	
-	//Paso2:  Declaramos una cadena de acceso publica llamada direcciÛn
+	//Paso2:  Declaramos una cadena de acceso publica llamada direcciÔøΩn
+	public String direccion;
 	
 	//Paso3:  Declaramos y asignamos un objeto privado float para los m2, le damos un valor
 	// por defecto
+	private float m2=120; 
 	
 	//Paso4:  Declaramos un constructor privado por defecto
+	private Casa() {};
 	
 	//Paso5:  Declaramos un constructor publico con los atributos de tipo primitivo
-	
+	public Casa(int numHabitaciones, int planta) {
+		this.numHabitaciones = numHabitaciones;
+		this.planta = planta;
+	}
+
 	//Paso6:  Declarmos un constructor con todos los tipos numericos
 	
+	public Casa(int numHabitaciones, int planta, float m2) {
+		this.numHabitaciones = numHabitaciones;
+		this.planta = planta;
+		this.m2 = m2;
+	}
+	
 	//Paso7:  Declaramos un constructor protegido con los datos de cadena
+	protected Casa(String direccion) {
+		this.direccion = direccion;
+	}
 	
-	//Paso8:  Declararemos los mÈtodos de acceso que sean necesarios para los atributos
-	
+	//Paso8:  Declararemos los mÔøΩtodos de acceso que sean necesarios para los atributos
+	public int getNumHabitaciones() {
+		return numHabitaciones;
+	}
+
+	public void setNumHabitaciones(int numHabitaciones) {
+		this.numHabitaciones = numHabitaciones;
+	}
+
+	public int getPlanta() {
+		return planta;
+	}
+
+	public void setPlanta(int planta) {
+		this.planta = planta;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public float getM2() {
+		return m2;
+	}
+
+	public void setM2(float m2) {
+		this.m2 = m2;
+	};		
 	
 	//Paso9:  Declaramos un metodo init sin parametros que invoque al constructor privado
-	// y retorne una instancia de objeto
+	// y retorne una instancia de objeto (DEBE SER EST√ÅTICO)
+	//¬°Claro! El objetivo de ese paso es crear un patr√≥n llamado "Factory Method"
+	//o, m√°s com√∫nmente, "Singleton" (si solo permites una instancia),
+	//que te permite crear objetos de la clase a trav√©s de un m√©todo est√°tico, en lugar de invocar directamente el constructor.
+
+	public static Casa init() {
+	    // Aqu√≠ es donde se invoca el constructor privado Casa()
+		Casa privateCasa = new Casa();
+		return privateCasa;
+		
+		//Gemini hace: return new Casa();
+	}
 	
 	
-	//Paso10:  Declaramos un metodo modificar que modifique todos los atributos del objeto
+	//Paso10:  Declaramos un metodo modificar que modifique todos los atributos del objeto: modificar()
 	
 	
 	
 	//Paso11:  Declaramos un metodo modificar que modifique todos los atributos del objeto
-	// pas·ndole como par·metro ˙nicamente otro objeto Casa
+	// pasÔøΩndole como parÔøΩmetro ÔøΩnicamente otro objeto Casa
 	
 	
 	
 	
-	//Paso12:  Sobreescribir el mÈtodo toString para que se muestren los atributos de esta clase
+	//Paso12:  Sobreescribir el mÔøΩtodo toString para que se muestren los atributos de esta clase
 	
 }
