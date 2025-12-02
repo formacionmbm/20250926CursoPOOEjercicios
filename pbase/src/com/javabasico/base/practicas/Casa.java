8dd7488126ed6001bccfd8578f54a07f90440579
kisho-stack <jorgesc1903@gmail.com>
2025-12-02 11:17:36 +0100

    Subiendo cambios en casa.java

--- pbase/src/com/javabasico/base/practicas/Casa.java
+++ pbase/src/com/javabasico/base/practicas/Casa.java
@@ -3,37 +3,92 @@ package com.javabasico.base.practicas;
 
 public class Casa {
 	//Paso1: Inicializamos las variables de entero: numHabitaciones y planta por defecto a 0
-	
-	//Paso2:  Declaramos una cadena de acceso publica llamada dirección
-	
+	private int numHabitaciones=0;
+    private int planta=0;
+
+    //Paso2:  Declaramos una cadena de acceso publica llamada direcciï¿½n
+	public String direccion;
 	//Paso3:  Declaramos y asignamos un objeto privado float para los m2, le damos un valor
 	// por defecto
+    private Integer m2=0;
 	
 	//Paso4:  Declaramos un constructor privado por defecto
-	
+    private Casa(){
+    }
+
 	//Paso5:  Declaramos un constructor publico con los atributos de tipo primitivo
-	
+    public Casa(int numHabitaciones, int planta){
+        this.numHabitaciones = numHabitaciones;
+        this.planta = planta;
+    }
 	//Paso6:  Declarmos un constructor con todos los tipos numericos
+    public Casa(int numHabitaciones, int planta, Integer m2){
+        this.numHabitaciones = numHabitaciones;
+        this.planta = planta;
+        this.m2 = m2;
+    }
 	
 	//Paso7:  Declaramos un constructor protegido con los datos de cadena
-	
-	//Paso8:  Declararemos los métodos de acceso que sean necesarios para los atributos
-	
-	
-	//Paso9:  Declaramos un metodo init sin parametros que invoque al constructor privado
+    protected Casa(String direccion){
+        this.direccion = direccion;
+    }
+
+	//Paso8:  Declararemos los mï¿½todos de acceso que sean necesarios para los atributos
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public int getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(int planta) {
+        this.planta = planta;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getM2() {
+        return m2;
+    }
+
+    public void setM2(Integer m2) {
+        this.m2 = m2;
+    }
+
+
+    //Paso9:  Declaramos un metodo init sin parametros que invoque al constructor privado
 	// y retorne una instancia de objeto
-	
+    public static Casa init() {
+        Casa casa1 = new Casa();
+        return casa1;
+    }
 	
 	//Paso10:  Declaramos un metodo modificar que modifique todos los atributos del objeto
-	
-	
+    public static void modificar (int numHabitaciones, int planta, String direccion, Integer m2){
+        this.numHabitaciones = numHabitaciones;
+        this.planta = planta;
+        this.direccion = direccion;
+        this.m2=m2;
+    }
 	
 	//Paso11:  Declaramos un metodo modificar que modifique todos los atributos del objeto
-	// pasándole como parámetro únicamente otro objeto Casa
+	// pasï¿½ndole como parï¿½metro ï¿½nicamente otro objeto Casa
 	
 	
 	
 	
-	//Paso12:  Sobreescribir el método toString para que se muestren los atributos de esta clase
+	//Paso12:  Sobreescribir el mï¿½todo toString para que se muestren los atributos de esta clase
 	
 }
