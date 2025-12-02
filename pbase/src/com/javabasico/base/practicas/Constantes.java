da95bfbd4eb55d3d7f774322551e501ac8b1d49c
MDVeinte <maurodesruelles@gmail.com>
2025-12-02 12:31:52 +0100

    Y mas cosas

--- pbase/src/com/javabasico/base/practicas/Constantes.java
+++ pbase/src/com/javabasico/base/practicas/Constantes.java
@@ -1,11 +1,10 @@
 package com.javabasico.base.practicas;
 
-public class Constantes {
-	
+public class Constantes {	
 	public static final String SUMA = "SUMA: ";
+	
 	//A�adir las constantes necesarias para las otras operaciones
 	public static final String RESTA = "RESTA: ";
 	public static final String MULTIPLICACION = "MULTIPLICACION: ";
-	public static final String DIVISION = "DIVISION: ";
-	
+	public static final String DIVISION = "DIVISION: ";	
 }
