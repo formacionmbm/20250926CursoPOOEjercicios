d2fbee7dfcff9863df7d619c465b1475bfa7eb56
fnavas <fnavas.dev@gmail.com>
2025-12-09 11:05:30 +0100

    Refactor constants and exception handling in the codebase

--- pexceptions/src/com/proyecto/common/exception/CodeErrors.java
+++ pexceptions/src/com/proyecto/common/exception/CodeErrors.java
@@ -2,11 +2,11 @@ package com.proyecto.common.exception;
 
 public class CodeErrors {
 	//Paso 1. Constante de cadena String "NO_STRING" con valor ER_STR001	
-	
+	public static final String NO_STRING = "ER_STR001";
 	//Paso 2. Constante de cadena String "STRING_TOO_LONG" con valor ER_STR002	
-	
+	public static final String NO_SUFFIX = "ER_SUFFIX";
 	//Paso 3. Constante de cadena String "STRING_TOO_SHORT" con valor ER_STR003	
-	
+	public static final String NO_SUFFIX2 = "ER_SUFFIX2";
 	//Paso 4. Constante de cadena String "WORD_NOT_ALLOWED_FOUND" con valor ER_STR004	
-	
+	public static final String NO_SUFFIX3 = "ER_SUFFIX3";
 }
