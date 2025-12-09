213458ef7ddab62700fb9d159731b68dedd7766b
Mananas <Mananas@GET-15>
2025-12-09 13:49:23 +0100

    09/12

--- pexceptions/src/com/proyecto/common/exception/StringNotValidException.java
+++ pexceptions/src/com/proyecto/common/exception/StringNotValidException.java
@@ -1,14 +1,28 @@
 package com.proyecto.common.exception;
 
-public class StringNotValidException  {
+public class StringNotValidException extends Exception{
 	
 	//Paso 1.Indicar atributo protegido de tipo String llamado "code"
-
-			
+	protected String code;
+	protected String mensaje;		
+	
 	//Paso 2. Implementar un contructor, que tenga por parametro un valor para code
 	// y un mensaje para el constructor del padre
-
+	public StringNotValidException(String code, String mensaje) {
+		
+		// Llamamos al constructor de la clase padre (RuntimeException) con el mensaje
+			super(mensaje);
+		this.code = code;
+	}
 	
 	
 
 }
+
+
+
+
+
+
+
+
--- pexceptions/src/com/proyecto/common/util/ValidationString.java
+++ pexceptions/src/com/proyecto/common/util/ValidationString.java
@@ -8,32 +8,43 @@ import com.proyecto.common.exception.CodeErrors;
 import com.proyecto.common.exception.NotAllowedWordsException;
 import com.proyecto.common.exception.StringNotValidException;
 import com.proyecto.common.exception.StringTooLongException;
+import com.proyecto.common.exception.StringTooShortException;
+
 
 public class ValidationString {
 
-	// Paso 1. Implementar un método estático "validLenght"que valide
-	// si una cadena tiene mas tamañano de 25 posiciones y menos de 1 posición
-	// El método tiene que devolver void, utilizar excepciones, declarar las
+
+	
+	// Paso 1. Implementar un mï¿½todo estï¿½tico "validLenght"que valide
+	// si una cadena tiene mas tamaï¿½ano de 25 posiciones y menos de 1 posiciï¿½n
+	// El mï¿½todo tiene que devolver void, utilizar excepciones, declarar las
 	// necesarias
-		// Paso 1.1. Validar si la cadena no es nula y no esta vacía
-		// Paso 1.2 Validar si es mayor que el tamaño maximo (Constantes)
-		// Paso 1.3 Validar si es menor que el tamaño minimo (Constantes)
-		
+	
+	private static final String[] PROHIBITED_WORDS = {"spam", "hack", "prohibida"};
+		// Paso 1.1. Validar si la cadena no es nula y no esta vacï¿½a
+		// Paso 1.2 Validar si es mayor que el tamaï¿½o maximo (Constantes)
+		// Paso 1.3 Validar si es menor que el tamaï¿½o minimo (Constantes)	
 		// Paso 1.4 Validar si no contiene palabras no permitidas;
 	
-	
+	public static void validLenght(String value) 
+			throws StringNotValidException, StringTooLongException, StringTooShortException, NotAllowedWordsException {
+			
+		validLenght(value, Constantes.TAMANIO_MAX, Constantes.TAMANIO_MIN);
+	}
 
-	// Paso 2: Sobrecargar el método anterior para que realize las
-	// validaciones con dos valores máximo y mínimo pasados por parámetros
+	// Paso 2: Sobrecargar el mï¿½todo anterior para que realize las
+	// validaciones con dos valores mï¿½ximo y mï¿½nimo pasados por parï¿½metros
 	
-		// Paso 1.1. Validar si la cadena no es nula y no esta vacía
-		// Paso 1.2 Validar si es mayor que el tamaño maximo (Constantes)
-		// Paso 1.3 Validar si es menor que el tamaño minimo (Constantes)
+		// Paso 1.1. Validar si la cadena no es nula y no esta vacï¿½a
+		// Paso 1.2 Validar si es mayor que el tamaï¿½o maximo (Constantes)
+		// Paso 1.3 Validar si es menor que el tamaï¿½o minimo (Constantes)
 		// Paso 1.4 Validar si no contiene palabras no permitidas;		
 		
+	public static void validLenght(String value, int maxLength, int minLength) 
+			throws StringNotValidException, StringTooLongException, StringTooShortException, NotAllowedWordsException {
+
+		
+	// Paso 3. Si alguna funcionalidad se repitiese extraerla en un mï¿½todo privado
 	
 
-	// Paso 3. Si alguna funcionalidad se repitiese extraerla en un método privado
-	
-
-}
+}}
