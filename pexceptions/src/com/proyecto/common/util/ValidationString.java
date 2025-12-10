5bb1ba4dccf3722b5f616ee870445d6a8f60fe73
formacionmbm <formacionmbm@gmail.com>
2025-12-10 11:04:40 +0100

    se implementa los métodos de validación

--- pexceptions/src/com/proyecto/common/util/ValidationString.java
+++ pexceptions/src/com/proyecto/common/util/ValidationString.java
@@ -15,37 +15,40 @@ public class ValidationString {
 	// El m�todo tiene que devolver void, utilizar excepciones, declarar las
 	// necesarias
 	public static void validLenght(String cadena) throws StringNotValidException{
-		// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
-		
-		if(cadena == null || cadena.trim().isEmpty()) throw new StringNotValidException(CodeErrors.NO_STRING,"La cadena es nula o vacía");
-		// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
-		if(cadena.length() > Constantes.TAMANIO_MAX)
-			throw new StringTooLongException(cadena.length(),CodeErrors.STRING_TOO_LONG, "La cadena es demasiado larga");
-		// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
-		if(cadena.length() < Constantes.TAMANIO_MIN)
-			throw new StringTooShortException(cadena.length(),CodeErrors.STRING_TOO_SHORT, "La cadena es demasiado corta");
-		
-		// Paso 1.4 Validar si no contiene palabras no permitidas;
-		NotAllowedWord[] words=NotAllowedWord.values();
-		for(NotAllowedWord word:words) {
-			if(cadena.toUpperCase().contains(word.toString()))
-				throw new NotAllowedWordsException(word,CodeErrors.WORD_NOT_ALLOWED_FOUND, "La cadena contiene palabra no permitida: "+ word.toString());
-		}
+		validate(cadena, Constantes.TAMANIO_MAX, Constantes.TAMANIO_MIN);
 	
 	} //declaración método
 	
 
 	// Paso 2: Sobrecargar el m�todo anterior para que realize las
 	// validaciones con dos valores m�ximo y m�nimo pasados por par�metros
-	
-		// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
-		// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
-		// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
-		// Paso 1.4 Validar si no contiene palabras no permitidas;		
-		
-	
+	public static void validLenght(String cadena, int tamanio_max, int tamanio_min) throws StringNotValidException{
+		validate(cadena, tamanio_max, tamanio_min);
+	}
 
 	// Paso 3. Si alguna funcionalidad se repitiese extraerla en un m�todo privado
+
+	private static void validate(String cadena, int tamanio_max, int tamanio_min)
+			throws StringNotValidException {
+		// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
+		if(cadena == null || cadena.trim().isEmpty()) throw new StringNotValidException(CodeErrors.NO_STRING,"La cadena es nula o vacía");
+		
+		// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
+		if(cadena.length() > tamanio_max)
+			throw new StringTooLongException(cadena.length(),CodeErrors.STRING_TOO_LONG, "La cadena es demasiado larga");
+		// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
+		if(cadena.length() < tamanio_min)
+			throw new StringTooShortException(cadena.length(),CodeErrors.STRING_TOO_SHORT, "La cadena es demasiado corta");
+		
+		// Paso 1.4 Validar si no contiene palabras no permitidas;		
+		NotAllowedWord[] words=NotAllowedWord.values();
+		for(NotAllowedWord word:words) {
+			if(cadena.toUpperCase().contains(word.toString()))
+				throw new NotAllowedWordsException(word,CodeErrors.WORD_NOT_ALLOWED_FOUND, "La cadena contiene palabra no permitida: "+ word.toString());
+		}
+	}
+
+	
 	
 	
 }
